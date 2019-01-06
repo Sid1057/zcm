8fa3d57129fb88152ff587b7cf173de704bf4d3b
Jonathan Bendes <jonathan.bendes@gmail.com>
2019-01-05 19:26:04 -0500

    Including byte size

--- zcm/zcm_base_types.h
+++ zcm/zcm_base_types.h
@@ -1,20 +1,20 @@
 #ifndef __ZCM_BASE_TYPES__
 #define __ZCM_BASE_TYPES__ \
-X( uint8_t,   zuint8_t) \
-X(  int8_t,    zint8_t) \
-X(uint16_t,  zuint16_t) \
-X( int16_t,   zint16_t) \
-X(uint32_t,  zuint32_t) \
-X( int32_t,   zint32_t) \
-X(uint64_t,  zuint64_t) \
-X( int64_t,   zint64_t) \
-X(   float, zfloat32_t) \
-X(  double, zfloat64_t) \
+X(1,  uint8_t,   zuint8_t) \
+X(1,   int8_t,    zint8_t) \
+X(2, uint16_t,  zuint16_t) \
+X(2,  int16_t,   zint16_t) \
+X(4, uint32_t,  zuint32_t) \
+X(4,  int32_t,   zint32_t) \
+X(8, uint64_t,  zuint64_t) \
+X(8,  int64_t,   zint64_t) \
+X(4,    float, zfloat32_t) \
+X(8,   double, zfloat64_t) \
 
 #include <stdint.h>
 #include <float.h>
 
-#define X(NATIVE_TYPE, ZCM_BASE_TYPE) \
+#define X(_, NATIVE_TYPE, ZCM_BASE_TYPE) \
     typedef NATIVE_TYPE ZCM_BASE_TYPE
 #undef X
 
