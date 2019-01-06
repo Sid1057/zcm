5fa785629f072508876d33bc20cf9c4631e90a78
Jonathan Bendes <jonathan.bendes@gmail.com>
2019-01-05 19:00:59 -0500

    Added base types

---
+++ zcm/zcm_base_types.h
@@ -0,0 +1,18 @@
+#ifndef __ZCM_BASE_TYPES__
+#define __ZCM_BASE_TYPES__
+
+#include <stdint.h>
+#include <float.h>
+
+typedef  uint8_t   zuint8_t
+typedef   int8_t    zint8_t
+typedef uint16_t  zuint16_t
+typedef  int16_t   zint16_t
+typedef uint32_t  zuint32_t
+typedef  int32_t   zint32_t
+typedef uint64_t  zuint64_t
+typedef  int64_t   zint64_t
+typedef    float zfloat32_t
+typedef   double zfloat64_t
+
+#endif // __ZCM_BASE_TYPES__
