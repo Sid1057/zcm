b70d3bd538e9f2a6a327c6c70118b823386bc7e4
Jonathan Bendes <jonathan.bendes@gmail.com>
2019-01-05 19:24:32 -0500

    Better formatting

--- zcm/zcm_base_types.h
+++ zcm/zcm_base_types.h
@@ -1,18 +1,21 @@
 #ifndef __ZCM_BASE_TYPES__
-#define __ZCM_BASE_TYPES__
+#define __ZCM_BASE_TYPES__ \
+X( uint8_t,   zuint8_t) \
+X(  int8_t,    zint8_t) \
+X(uint16_t,  zuint16_t) \
+X( int16_t,   zint16_t) \
+X(uint32_t,  zuint32_t) \
+X( int32_t,   zint32_t) \
+X(uint64_t,  zuint64_t) \
+X( int64_t,   zint64_t) \
+X(   float, zfloat32_t) \
+X(  double, zfloat64_t) \
 
 #include <stdint.h>
 #include <float.h>
 
-typedef  uint8_t   zuint8_t
-typedef   int8_t    zint8_t
-typedef uint16_t  zuint16_t
-typedef  int16_t   zint16_t
-typedef uint32_t  zuint32_t
-typedef  int32_t   zint32_t
-typedef uint64_t  zuint64_t
-typedef  int64_t   zint64_t
-typedef    float zfloat32_t
-typedef   double zfloat64_t
+#define X(NATIVE_TYPE, ZCM_BASE_TYPE) \
+    typedef NATIVE_TYPE ZCM_BASE_TYPE
+#undef X
 
 #endif // __ZCM_BASE_TYPES__
